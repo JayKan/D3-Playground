35d648423d0a830cdca60ba17192709e2fc3c97c
Jay Kan <leicasper@gmail.com>
2017-03-01 12:54:34 -0800

    example(tree): add collapsible tree example

--- charts/bar-chart/barChart.js
+++ charts/bar-chart/barChart.js
@@ -1,7 +1,6 @@
 ;(function(global, d3) {
   'use strict';
-  
-  console.log('BarChart Init()..');
+
   var margin = { top: 20, right: 20, bottom: 30, left: 40 },
       width = 960 - margin.left - margin.right,
       height = 500 - margin.top - margin.bottom;
